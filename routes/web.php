56033debe08809e9e4ca38f4ac9b82ca50b60df0
= <ilhamkartolonggeng21@gmail.com>
2022-06-29 07:04:52 +0700

    membuat route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,9 +15,22 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::middleware('auth')->group(function () {
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+    // Route::get('/user', [App\Http\Controllers\UserController::class, 'index'])->name('user.index');
+    Route::resource('/user', App\Http\Controllers\UserController::class);
+    Route::resource('/customers', App\Http\Controllers\CustomerController::class);
+    Route::resource('/categories', App\Http\Controllers\CategoryController::class);
+    Route::resource('/vehicles', App\Http\Controllers\VehicleController::class);
+    Route::resource('/vehiclesIn', App\Http\Controllers\VehicleInController::class);
+    Route::resource('/vehiclesOut', App\Http\Controllers\VehicleOutController::class);
+
+    Route::get('reports/index', [App\Http\Controllers\ReportController::class, 'index'])->name('reports.index');
+
+});
