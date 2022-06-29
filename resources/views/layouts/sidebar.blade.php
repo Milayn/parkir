1fc84b685b8d5c7b79002e3032e1a3aa54ea4711
= <ilhamkartolonggeng21@gmail.com>
2022-06-29 17:43:06 +0700

    Update Bootstrap

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -20,40 +20,40 @@
                 </div>
 
                 <div class="nav-item has-sub {{ request()->routeIs('user*')  ? 'open' : ''}}">
-                    <a href="javascript:void(0)"><i class="ik ik-user"></i><span>Manage Admins </span> </a>
+                    <a href="javascript:void(0)"><i class="ik ik-user"></i><span>Admins</span> </a>
                     <div class="submenu-content">
-                        <a href="{{ route('user.create') }}" class="menu-item {{ request()->routeIs('user.create') ? 'active' : '' }}">Create</a>
-                        <a href="{{ route('user.index') }}" class="menu-item  {{ request()->routeIs('user.index') ? 'active' : '' }}">List</a>
+                        <a href="{{ route('user.create') }}" class="menu-item {{ request()->routeIs('user.create') ? 'active' : '' }}">Tambah Daftar Admin</a>
+                        <a href="{{ route('user.index') }}" class="menu-item  {{ request()->routeIs('user.index') ? 'active' : '' }}">Daftar Admin</a>
                     </div>
                 </div>
                 <div class="nav-item has-sub {{ request()->routeIs('customers*')  ? 'open' : ''}}">
-                    <a href="javascript:void(0)"><i class="ik ik-users"></i><span>Manage Customers </span> </a>
+                    <a href="javascript:void(0)"><i class="ik ik-users"></i><span>Pengunjung </span> </a>
                     <div class="submenu-content">
-                        <a href="{{ route('customers.create') }}" class="menu-item  {{ request()->routeIs('customers.create') ? 'active' : '' }}">Create</a>
-                        <a href="{{ route('customers.index') }}" class="menu-item  {{ request()->routeIs('customers.index') ? 'active' : '' }}">List</a>
+                        <a href="{{ route('customers.create') }}" class="menu-item  {{ request()->routeIs('customers.create') ? 'active' : '' }}">Tambah Daftar Pengunjung </a>
+                        <a href="{{ route('customers.index') }}" class="menu-item  {{ request()->routeIs('customers.index') ? 'active' : '' }}">Daftar Pengunjung</a>
                     </div>
                 </div>
                 <div class="nav-lavel">UI Element</div>
                 <div class="nav-item has-sub {{ request()->routeIs('categories*')  ? 'open' : ''}}">
-                    <a href="#"><i class="ik ik-box"></i><span>Manage Category</span></a>
+                    <a href="#"><i class="ik ik-box"></i><span>Kategori</span></a>
                     <div class="submenu-content">
                         <a href="{{ route('categories.create') }}" class="menu-item  {{ request()->routeIs('categories.create') ? 'active' : '' }}">Create</a>
                         <a href="{{ route('categories.index') }}" class="menu-item  {{ request()->routeIs('categories.index') ? 'active' : '' }}">List</a>
                     </div>
                 </div>
                 <div class="nav-item has-sub {{ request()->routeIs('vehicles*')  ? 'open' : ''}}">
-                    <a href="#"><i class="ik ik-truck"></i><span>Register Vehicles</span> </a>
+                    <a href="#"><i class="ik ik-truck"></i><span>Daftar Kendaraan</span> </a>
                     <div class="submenu-content">
-                        <a href="{{ route('vehicles.create') }}" class="menu-item  {{ request()->routeIs('vehicles.create') ? 'active' : '' }}">Create</a>
-                        <a href="{{ route('vehicles.index') }}" class="menu-item  {{ request()->routeIs('vehicles.index') ? 'active' : '' }}">List</a>
+                        <a href="{{ route('vehicles.create') }}" class="menu-item  {{ request()->routeIs('vehicles.create') ? 'active' : '' }}">Tambah Daftar Kendaraan</a>
+                        <a href="{{ route('vehicles.index') }}" class="menu-item  {{ request()->routeIs('vehicles.index') ? 'active' : '' }}">Daftar Kendaraan</a>
                     </div>
                 </div>
 
                 <div class="nav-item has-sub {{ request()->routeIs('vehiclesIn*') || request()->routeIs('vehiclesOut*')  ? 'open' : ''}}">
-                    <a href="#"><i class="ik ik-gitlab"></i><span>Manage Vehicles</span> </a>
+                    <a href="#"><i class="ik ik-gitlab"></i><span>Informasi Kendaraan</span> </a>
                     <div class="submenu-content">
-                        <a href="{{ route('vehiclesIn.index') }}" class="menu-item  {{ request()->routeIs('vehiclesIn*') ? 'active' : '' }}">In Vehicles</a>
-                        <a href="{{ route('vehiclesOut.index') }}" class="menu-item  {{ request()->routeIs('vehiclesOut*') ? 'active' : '' }}">Out Vehicle</a>
+                        <a href="{{ route('vehiclesIn.index') }}" class="menu-item  {{ request()->routeIs('vehiclesIn*') ? 'active' : '' }}">Kendaraan Masuk</a>
+                        <a href="{{ route('vehiclesOut.index') }}" class="menu-item  {{ request()->routeIs('vehiclesOut*') ? 'active' : '' }}">Kendaraan Keluar</a>
                     </div>
                 </div>
 
